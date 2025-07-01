a4518642b4b7cc97000a086f1ba3967186c4dec0
be302_first <sirliyevaaysun93@gmail.com>
2025-07-01 13:39:27 +0400

    entities

---
+++ src/Core/MiniECommerce.Domain/Entities/Image.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniECommerce.Domain.Entities;
+
+public class Image : BaseEntity
+{
+    public string ImageUrl { get; set; }
+    public int ProductId { get; set; }
+    public Product Product { get; set; }
+}
