634dedc4e7c0e43af39724b644e8a9afe242bc77
be302_first <sirliyevaaysun93@gmail.com>
2025-07-07 01:53:04 +0400

    Initial commit

---
+++ src/Presentation/MiniECommerce.WebApi/Controllers/EmailController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiniECommerce.Application.Abstracts.Services;
+using MiniECommerce.Application.DTOs.Email;
+
+namespace MiniECommerce.WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EmailController : ControllerBase
+{
+    private readonly IEmailService _emailService;
+
+    public EmailController(IEmailService emailService)
+    {
+        _emailService = emailService;
+    }
+
+    [HttpPost("send-test")]
+    public async Task<IActionResult> SendTestEmail()
+    {
+        var emailDto = new EmailDto
+        {
+            To = "sunahacker01@gmail.com", 
+            Subject = "Test Email from MiniECommerce",
+            Body = "<strong>Bu email SMTP ilə göndərildi!</strong>"
+        };
+
+        try
+        {
+            await _emailService.SendAsync(emailDto);
+            return Ok("Email uğurla göndərildi!");
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Email göndərilərkən xəta baş verdi: {ex.Message}");
+        }
+    }
+}
